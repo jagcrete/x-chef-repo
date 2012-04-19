9fc40d182ff8de4b5f905675ee41d5ba3be24b5b
jarrad <jarrad@jagcrete.com>
2012-04-19 17:58:57 -0400

    update cookbook jagcrete to not use symbols

--- cookbooks/jagcrete/recipes/default.rb
+++ cookbooks/jagcrete/recipes/default.rb
@@ -18,26 +18,28 @@ ids.each do |item|
   # get a user item out of the bag
   id = data_bag_item( bag_name, item )
 
+  username = id['id']
+
   # create a user account using the data
   # from the data bag
-  user_account id[:user] do
+  user_account username do
     # a comment about the user
-    comment     id[:comment] || id[:user]
+    comment     id['comment'] || username
     # an array of ssh keys
-    ssh_keys    id[:ssh_key]
+    ssh_keys    id['ssh_key']
     # we don't want to generate a priv/pub key for this user
     ssh_keygen  false
     # if we have a shadow password, set it
     # openssl passwd -1
-    password    id[:shadow] unless id[:shadow].nil?
+    password    id['shadow'] unless id['shadow'].nil?
   end
 
   # add user to the configured groups
   # groups is expected to be an array
-  id[:groups].each do |g|
+  id['groups'].each do |g|
     group g do
-      members [ id[:user] ]
+      members [ username ]
     end
-  end unless id[:groups].nil?
+  end unless id['groups'].nil?
 
 end unless ids.nil?
